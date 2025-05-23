7cfb94b35955046cc268c27996e0e2fbdf0209e3
Tom Schulz <91066892+tomschulz02@users.noreply.github.com>
2025-05-23 14:44:21 +0200

    Basic navigation with mobile responsiveness
    - different layouts are used depending on screen width
    -- if user is on desktop, navigation is at the top of the screen in header
    -- if user is on mobile, navigation is on the bottom of the screen

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ How to run the app:
 
 1. `cd frontend`
 2. `npm install`
-3. `npm start` to run a dev instance; `npm run build && serve -s build` to deploy a local instance
+3. `npm start` to run a dev instance; `npm run build` and then `serve -s build` to deploy a local instance
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Home from "./pages/Home";
 import Map from "./pages/Map";
@@ -6,13 +6,16 @@ import Camera from "./pages/Camera";
 import Feed from "./pages/Feed";
 import Profile from "./pages/Profile";
 import Login from "./pages/Login";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
+import useIsMobile from "./hooks/useIsMobile";
 
 function App() {
+	const isMobile = useIsMobile();
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/">
+				<Route path="/" element={isMobile ? <MobileLayout /> : <WebLayout />}>
+					<Route path="/" element={<Home />} />
 					<Route path="/home" element={<Home />} />
 					<Route path="/map" element={<Map />} />
 					<Route path="/camera" element={<Camera />} />
@@ -26,3 +29,80 @@ function App() {
 }
 
 export default App;
+
+function WebLayout() {
+	return (
+		<>
+			<div className="web-header">
+				<div className="web-heading">BushBuddy</div>
+				<div className="web-navbar">
+					<Link to={"/home"} className="web-navlink">
+						Home
+					</Link>
+					<Link to={"/map"} className="web-navlink">
+						Map
+					</Link>
+					<Link to={"/camera"} className="web-navlink">
+						Camera
+					</Link>
+					<Link to={"/feed"} className="web-navlink">
+						Feed
+					</Link>
+					<Link to={"/profile"} className="web-navlink">
+						Profile
+					</Link>
+					<Link to={"/login"} className="web-navlink">
+						Login
+					</Link>
+				</div>
+			</div>
+			<main id="app">
+				<Outlet />
+			</main>
+		</>
+	);
+}
+
+function MobileLayout() {
+	const [currentTab, setCurrentTab] = useState("home");
+
+	return (
+		<>
+			<main id="app">
+				<Outlet />
+			</main>
+			<div className="mobile-navbar">
+				<Link
+					to={"/home"}
+					className={`mobile-navlink ${currentTab === "home" ? "active" : ""}`}
+					onClick={() => setCurrentTab("home")}>
+					Home
+				</Link>
+				<Link
+					to={"/map"}
+					className={`mobile-navlink ${currentTab === "map" ? "active" : ""}`}
+					onClick={() => setCurrentTab("map")}>
+					Map
+				</Link>
+				<Link
+					to={"/camera"}
+					className={`mobile-navlink ${currentTab === "camera" ? "active" : ""}`}
+					onClick={() => setCurrentTab("camera")}>
+					Camera
+				</Link>
+				<Link
+					to={"/feed"}
+					className={`mobile-navlink ${currentTab === "feed" ? "active" : ""}`}
+					onClick={() => setCurrentTab("feed")}>
+					Feed
+				</Link>
+				<Link
+					to={"/profile"}
+					className={`mobile-navlink ${currentTab === "profile" ? "active" : ""}`}
+					onClick={() => setCurrentTab("profile")}>
+					Profile
+				</Link>
+			</div>
+		</>
+	);
+}
