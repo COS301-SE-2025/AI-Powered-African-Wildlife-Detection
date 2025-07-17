7599c326e26edd3dab6b1910297f44b0d10ad565
EsterhuizenRuan <ruan.esterhuizen123@gmail.com>
2025-07-17 19:40:49 +0200

    small map fixes

--- frontend/src/pages/MapPage.js
+++ frontend/src/pages/MapPage.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import { MapContainer, TileLayer, LayersControl } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, LayersControl } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import './MapPage.css';
 
 const MapPage = () => {
-  const position = [-25.8812222, 28.291611111111113]; //starting location, should be init to user location
+  const [currentPosition, setCurrentPosition] = useState(null);
+  const defaultPosition  = [-25.8812222, 28.291611111111113]; //starting location, should be init to user location
+
+  delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
+useEffect(() => {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setCurrentPosition([
+            position.coords.latitude,
+            position.coords.longitude
+          ]);
+        },
+        (error) => {
+          console.error("Geolocation error:", error);
+        }
+      );
+    }
+  }, []);
 
   return (
     <Container className="map-page">
       <MapContainer
-        center={position}
-        zoom={13}
+        center={currentPosition || defaultPosition}
+        zoom={16}
         scrollWheelZoom={true}
         style={{ height: '100%', width: '100%' }}
         className='map-container'
@@ -30,6 +55,11 @@ const MapPage = () => {
             />
           </LayersControl.BaseLayer>
         </LayersControl>
+
+        {currentPosition && (
+          <Marker position={currentPosition} />
+        )}
+
       </MapContainer>
     </Container>
   );
